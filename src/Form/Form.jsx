c5a4f6d98ef6a333a4ee74f6e4b949465ca19d8b
Flávia Reis <flaviarster@gmail.com>
2024-04-26 11:31:26 -0300

    Treinando props

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,20 +1,13 @@
-import React from 'react'
-import { Input } from './Input'
-import { Button } from './Button'
+import React from 'react';
+import { Input } from './Input';
+import { Button } from './Button';
 
 export const Form = () => {
   return (
     <form>
-      <p>
-        <label htmlFor="nome">Nome</label>
-        <Input />
-      </p>
-      <p>
-        <label htmlFor="mail">Email</label>
-        <Input />
-      </p>
+      <Input id='mail' label='Email' required />
+      <Input id='password' type='password' label='Password' />
       <Button />
     </form>
-  )
-}
-
+  );
+};
--- src/Form/Input.jsx
+++ src/Form/Input.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-export const Input = () => {
+export const Input = ({ id, label, ...props }) => {
+  console.log(props)
   return (
-    <input type="text" />
+    <div style={{margin: '1rem 0'}}>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type="text" {...props} />
+    </div>
   )
 }
 
