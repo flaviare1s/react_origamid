cdbf2442e8a9e6ae89add8e93afc0f8baaba6cb7
Flávia Reis <flaviarster@gmail.com>
2024-04-28 10:31:03 -0300

    Fiz exercício

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { ProductDetails } from './Desafio Produtos/ProductDetails';
 
 
 export const App = () => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(null)
+
+  async function handleClick(event) {
+    setLoading(true)
+    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`)
+    const json = await response.json()
+    setData(json)
+    setLoading(false)
+  }
+  
   return (
-    <>
-      <ul>
-        <li><a href={'/home'}>Home</a></li>
-        <li><a href={'/produtos'}>Produtos</a></li>
-      </ul>
-    </>
-  )
-}
-
-
+    <div>
+      <button style={{margin: '5rem'}} onClick={handleClick}>notebook</button>
+      <button style={{margin: '5rem'}} onClick={handleClick}>smartphone</button>
+      <button style={{margin: '5rem'}} onClick={handleClick}>tablet</button>
+      {loading && <p>Carregando...</p>}
+      {!loading && data && <ProductDetails data={data}/>}
+    </div>
+  );
+};
