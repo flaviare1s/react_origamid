2cd57668ef31641f4cb3b42ea9a15e2aa3079ce6
Flávia Reis <flaviarster@gmail.com>
2024-05-01 10:25:38 -0300

    Fiz o componente de select

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import { Input } from './Form/Input';
+import { Select } from './Form/Select';
 
 export const App = () => {
   const [nome, setNome] = useState('')
   const [email, setEmail] = useState('')
+  const [produto, setProduto] =useState('')
 
   return (
     <div>
       <Input id="nome" label="Nome" value={nome} setValue={setNome} />
       <Input id="email" label="Email" value={email} setValue={setEmail} required/>
+      <Select options={['Smartphone', 'Tablet']} value={produto} setValue={setProduto}/>
     </div>
   );
 };
